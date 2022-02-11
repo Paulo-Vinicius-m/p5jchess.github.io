cf8a0b49afd038533c31809c017d23c4b46a1844
PV-LINDO <65673077+PV-LINDO@users.noreply.github.com>
2022-02-11 19:04:39 -0300

    Add files via upload

---
+++ evaluation_function.js
@@ -0,0 +1,70 @@
+const bish = 315
+const knight = 300
+const queen = 900
+const rook = 500
+const pawn = 100
+var meupar = 0
+var paroponente = 0
+var casadobispo
+var casadopeao = []
+var cor
+
+// Função para avaliar funções "estáticas"
+export default function Eval (color, game) {
+
+    var evaluation = 0
+
+    // Verifica se o jogo acabou e retorna a avaliação da posição
+    if (game.moves().length ===0){
+        if (game.in_check()) {
+            return (-9999999);
+        }
+        else {
+                return 0;
+        }
+    }
+    // Conta as peças da engine e do jogador pra ver quem tem mais meterial
+    else {
+        for (let i = 0; i < 8; i=i+1) {
+            for (let j = 0; j < 8; j=j+1) {
+                if (game.board()[i][j] == null) { }
+                else {
+                    if (game.board()[i][j].color == color) {
+                        if (game.board()[i][j].type == 'q') { evaluation = evaluation + queen; }
+                        if (game.board()[i][j].type == 'n') { evaluation = evaluation + knight; }
+                        if (game.board()[i][j].type == 'b') { evaluation = evaluation + bish; meupar++; casadobispo = ((i+j)%2)===0}
+                        if (game.board()[i][j].type == 'r') { evaluation = evaluation + rook; }
+                        if (game.board()[i][j].type == 'p') { evaluation = evaluation + pawn; cor = ((i+j)%2)===0; casadopeao.push(cor)}
+                    }
+                    if (game.board()[i][j].color != color) {
+                        if (game.board()[i][j].type == 'q') { evaluation = evaluation - queen; }
+                        if (game.board()[i][j].type == 'n') { evaluation = evaluation - knight; }
+                        if (game.board()[i][j].type == 'b') { evaluation = evaluation - bish; paroponente++}
+                        if (game.board()[i][j].type == 'r') { evaluation = evaluation - rook; }
+                        if (game.board()[i][j].type == 'p') { evaluation = evaluation - pawn; }
+                    }
+                }
+            }
+        }
+        
+        // Dá uma valorizada no par de bispos ;) e vê se tem bispos ruins ou bons
+        if (meupar == 2){
+            evaluation = evaluation + 108 - casadopeao.length
+        }
+        else {
+            if (meupar == 1){
+                for (var k=0; k<casadopeao.length; k++){
+                    if (casadopeao[k] == casadobispo){evaluation--}
+                }
+            }
+        }
+        if (paroponente == 2){
+            evaluation = evaluation - 100
+        }
+
+        // Valoriza a mobilidade das peças
+        evaluation = evaluation + game.moves().length
+        
+        return evaluation
+    } 
+}
---
+++ main.js
@@ -0,0 +1,123 @@
+import {INPUT_EVENT_TYPE, COLOR, Chessboard, MARKER_TYPE} from "../libraries/cm-chessboard-master/src/cm-chessboard/Chessboard.js"
+import makemove from "/engine.js"
+import Eval from "/evaluation_function.js"
+import {Chess} from '/libraries/chess/chess.js'
+
+// Board => representação gráfica do tabuleiro e input handler
+// chess => classe que armazena a posição, lida com as regras do jogo, gera os lances que são possíveis e etc.
+
+// Declaração de variáveis
+var promotion
+var cor
+var config
+
+// Criação da representação do jogo
+const chess = new Chess()
+
+// Pega o valor da URL (ver o HTML) que diz com qual cor o usuário irá jogar
+if (get){
+    cor = get
+}
+else{
+    cor = 'white'
+}
+
+// Posições de mate em 3 para teste
+//chess.load('5r2/1p4n1/p3n3/2kp1pp1/8/P2B2P1/1P1N1PP1/2R2K2 b - - 1 30')
+//chess.load('2r2k2/1p1n1pp1/p2b2p1/8/2KP1PP1/P3N3/1P4N1/5R2 w - - 0 1')
+
+// Se a engine for jogar de brancas, fazer o lance antes de iniciar o tabuleiro
+if (cor[0] != chess.turn()) {
+    makemove(null, chess)
+}
+
+// Configurações da ilustração do tabuleiro: mudar o ponto de vista de acordo com as peças do jogador
+if (cor[0] =='w'){ 
+    config = {
+    responsive: true,
+    position: chess.fen(),
+    sprite: {url: "/libraries/cm-chessboard-master/assets/images/chessboard-sprite-staunty.svg"},
+    style: {moveFromMarker: undefined, moveToMarker: undefined},
+    orientation: COLOR.white
+    }
+}
+
+else{ 
+    cor = 'black'
+    config = {
+    responsive: true,
+    position: chess.fen(),
+    sprite: {url: "/libraries/cm-chessboard-master/assets/images/chessboard-sprite-staunty.svg"},
+    style: {moveFromMarker: undefined, moveToMarker: undefined},
+    orientation: COLOR.black}
+}
+
+// Iniciar a ilustração do tabuleiro e permitir a interação com ele
+const board = new Chessboard(document.getElementById("board"), config)
+board.enableMoveInput(inputHandler, COLOR.cor)
+
+function inputHandler(event) {
+    
+    // "limpa" o tabuleiro
+    console.log("event", event)
+    event.chessboard.removeMarkers(undefined, MARKER_TYPE.dot)
+    event.chessboard.removeMarkers(undefined, MARKER_TYPE.square)
+
+    // Verifica se o jogo ja encerrou. Caso tenha acabado, mostrar no console o histórico da partida
+    if(chess.game_over()){
+
+        if(chess.in_check()){
+            alert ('Xeque-mate!')
+        }
+        else{
+            alert ('Empate!')
+        }
+
+        return console.log(chess.pgn())
+    }
+
+    // Detecta se a interação é o início de uma jogada
+    if (event.type === INPUT_EVENT_TYPE.moveStart) {
+        const moves = chess.moves({square: event.square, verbose: true});
+        event.chessboard.addMarker(event.square, MARKER_TYPE.square)
+        for (const move of moves) {
+            event.chessboard.addMarker(move.to, MARKER_TYPE.dot)
+        }
+        return moves.length > 0
+    }
+    // Detecta se a interação é o final da jogada
+    else if (event.type === INPUT_EVENT_TYPE.moveDone) {
+        // Verifica se o lance é uma promoção e, caso seja, pergunta ao usuário pra qual peça ele vai promover
+        if(chess.get(event.squareFrom).type == 'p' && (event.squareTo[1] == 8 || event.squareTo[1] == 1)){
+            promotion = prompt('promote to q, r, b or n?')
+            move = {from: event.squareFrom, to: event.squareTo, promotion: promotion}
+            var result = chess.move(move)
+        }
+        else{
+        var move = {from: event.squareFrom, to: event.squareTo}
+        var result = chess.move(move)
+        }
+
+        // Atualiza a ilustração do tabuleiro
+        event.chessboard.setPosition(chess.fen())
+        event.chessboard.disableMoveInput()
+
+        // Verifica se o lance foi ilegal
+        if (!result) {
+            console.warn("invalid move", move)
+        }
+    }
+
+    // Verifica se o jogo acabou e, caso não, a engine faz seu lance
+    if (cor[0] != chess.turn()) {
+
+        event.chessboard.removeMarkers(undefined, MARKER_TYPE.square)
+        if (chess.moves()) {
+            setTimeout(() => {
+            makemove(event.chessboard, chess)
+            console.log('eval: ' + Eval(cor[0], chess)/100)}, 50)
+        }
+    }
+    
+    event.chessboard.enableMoveInput(inputHandler, COLOR.cor)
+}
