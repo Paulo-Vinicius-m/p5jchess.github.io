ec536dc082502044957ec5d310ea143e29d409b0
PV-LINDO <65673077+PV-LINDO@users.noreply.github.com>
2022-02-13 20:15:19 -0300

    Update evaluation_function.js

--- evaluation_function.js
+++ evaluation_function.js
@@ -1,23 +1,17 @@
-const bish = 315
-const knight = 300
-const queen = 900
-const rook = 500
-const pawn = 100
-var meupar = 0
-var paroponente = 0
-var casadobispo
-var casadobispoadversario
-var casadopeao = []
-var casadopeaoadversario = []
-var cor
-
 // Função para avaliar funções "estáticas"
 export default function Eval (color, game) {
 
+    var meupar = 0
+    var paroponente = 0
+    var casadobispo
+    var casadobispoadversario
+    var peao = []
+    var peaoadversario = []
+    var pieces = 0
     var evaluation = 0
 
     // Verifica se o jogo acabou e retorna a avaliação da posição
-    if (game.moves().length ===0){
+    if (game.moves().length === 0){
         if (game.in_checkmate()) {
             return (-9999999);
         }
@@ -32,51 +26,50 @@ export default function Eval (color, game) {
                 if (game.board()[i][j] == null) { }
                 else {
                     if (game.board()[i][j].color == color) {
-                        if (game.board()[i][j].type == 'q') { evaluation = evaluation + queen; }
-                        if (game.board()[i][j].type == 'n') { evaluation = evaluation + knight; }
-                        if (game.board()[i][j].type == 'b') { evaluation = evaluation + bish; meupar++; casadobispo = ((i+j)%2)===0}
-                        if (game.board()[i][j].type == 'r') { evaluation = evaluation + rook; }
-                        if (game.board()[i][j].type == 'p') { evaluation = evaluation + pawn; cor = ((i+j)%2)===0; casadopeao.push(cor)}
+                        if (game.board()[i][j].type == 'q') { evaluation = evaluation + 900; pieces++}
+                        if (game.board()[i][j].type == 'n') { evaluation = evaluation + 300; pieces++}
+                        if (game.board()[i][j].type == 'b') { evaluation = evaluation + 315; meupar++; casadobispo = ((i+j)%2); pieces++}
+                        if (game.board()[i][j].type == 'r') { evaluation = evaluation + 500; pieces++}
+                        if (game.board()[i][j].type == 'p') { evaluation = evaluation + 100; peao.push(((i+j)%2)); pieces++}
                     }
                     if (game.board()[i][j].color != color) {
-                        if (game.board()[i][j].type == 'q') { evaluation = evaluation - queen; }
-                        if (game.board()[i][j].type == 'n') { evaluation = evaluation - knight; }
-                        if (game.board()[i][j].type == 'b') { evaluation = evaluation - bish; paroponente++; casadobispoadversario = ((i+j)%2)===0}
-                        if (game.board()[i][j].type == 'r') { evaluation = evaluation - rook; }
-                        if (game.board()[i][j].type == 'p') { evaluation = evaluation - pawn; cor = ((i+j)%2)===0; casadopeaoadversario.push(cor)}
+                        if (game.board()[i][j].type == 'q') { evaluation = evaluation - 900; pieces++}
+                        if (game.board()[i][j].type == 'n') { evaluation = evaluation - 300; pieces++}
+                        if (game.board()[i][j].type == 'b') { evaluation = evaluation - 315; paroponente++; casadobispoadversario = ((i+j)%2); pieces++}
+                        if (game.board()[i][j].type == 'r') { evaluation = evaluation - 500; pieces++}
+                        if (game.board()[i][j].type == 'p') { evaluation = evaluation - 100; peaoadversario.push(((i+j)%2)); pieces++}
                     }
                 }
             }
         }
         
-        // Dá uma valorizada no par de bispos ;) e vê se tem bispos ruins ou bons
+        // Dá uma valorizada no par de bispos ;) e vê se tem bispos bons ou maus
         if (meupar == 2){
-            evaluation = evaluation + 100 - casadopeao.length
+            evaluation = evaluation + 100 - peao.length
         }
         else {
             if (meupar == 1){
-                for (var k=0; k<casadopeao.length; k++){
-                    if (casadopeao[k] == casadobispo){
+                for (var k=0; k<peao.length; k++){
+                    if (peao[k] == casadobispo){
                         evaluation--
                     }
                 }
             }
         }
         if (paroponente == 2){
-            evaluation = evaluation - 100 + casadopeaoadversario.length
+            evaluation = evaluation - 100 + peaoadversario.length
         }
         else {
             if (paroponente == 1){
-                for (var k=0; k<casadopeao.length; k++){
-                    if (casadopeaoadversario[k] == casadobispoadversario){
+                for (var k=0; k<peao.length; k++){
+                    if (peaoadversario[k] == casadobispoadversario){
                         evaluation++
                     }
                 }
             }
         }
-
-        // Valoriza a mobilidade das peças
-        evaluation = evaluation + game.moves().length
+        // Valoriza a mobilidade das peças mas incentiva as trocas pra evitar posições muito complexas
+        evaluation = (evaluation + game.moves().length) - pieces
         
         return evaluation
     } 
